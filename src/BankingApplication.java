1f1b83501748c8070cdee09049affd7fd7fa920a
Md. Habibuzzaman <mhzaman999@gmail.com>
2024-01-31 22:35:02 +0600

    Delete Account

--- src/BankingApplication.java
+++ src/BankingApplication.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class BankingApplication{
+public class BankingApplication {
 
     private static ArrayList<BankAccount> accounts = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
@@ -10,79 +10,79 @@ public class BankingApplication{
     public static void main(String[] args) {
         int choice;
 
-        do{
-            try{
-            System.out.println("\nActions : ");
-            System.out.println("1. Create a new Savings account");
-            System.out.println("2. Create a new Current account");
-            System.out.println("3. Create a new Salary account");
-            System.out.println("4. Display all accounts");
-            System.out.println("5. Update an account");
-            System.out.println("6. Delete an account");
-            System.out.println("7. Deposit an amount into your account");
-            System.out.println("8. Withdraw an amount from your account");
-            System.out.println("9. Search for account");
-            System.out.println("10. Exit");
+        do {
+            try {
+                System.out.println("\nActions : ");
+                System.out.println("1. Create a new Savings account");
+                System.out.println("2. Create a new Current account");
+                System.out.println("3. Create a new Salary account");
+                System.out.println("4. Display all accounts");
+                System.out.println("5. Update an account");
+                System.out.println("6. Delete an account");
+                System.out.println("7. Deposit an amount into your account");
+                System.out.println("8. Withdraw an amount from your account");
+                System.out.println("9. Search for account");
+                System.out.println("10. Exit");
 
-            choice = scanner.nextInt();
+                choice = scanner.nextInt();
 
-            switch(choice){
-                case 1:
-                    createSavingsAccount();
-                    break;
+                switch (choice) {
+                    case 1:
+                        createSavingsAccount();
+                        break;
 
-                case 2:
-                    createCurrentAccount();
-                    break;
+                    case 2:
+                        createCurrentAccount();
+                        break;
 
-                case 3:
-                    createSalaryAccount();
-                    break;
+                    case 3:
+                        createSalaryAccount();
+                        break;
 
-                case 4:
-                    displayAccounts();
-                    break;
-                
-                case 5: 
-                    updateAcount();
-                    break;
+                    case 4:
+                        displayAccounts();
+                        break;
 
-                case 6: 
-                    deleteAcount();
-                    break;
-                
-                case 7: 
-                    deposit();
-                    break;
+                    case 5:
+                        updateAcount();
+                        break;
 
-                case 8:
-                    withdraw();
-                    break;
+                    case 6:
+                        deleteAcount();
+                        break;
 
-                case 9:
-                    searchAccount();
-                    break;
-                
-                case 10:
-                    break;
+                    case 7:
+                        deposit();
+                        break;
 
-                default:
-                    System.out.println("Invalid choice. Please enter a number between 1-10");
+                    case 8:
+                        withdraw();
+                        break;
+
+                    case 9:
+                        searchAccount();
+                        break;
+
+                    case 10:
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice. Please enter a number between 1-10");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number");
+                scanner.nextLine();
+                choice = -1;
+            } catch (Exception e) {
+                System.out.println("An unexpected error occurred. Please try again");
+                scanner.nextLine();
+                choice = -1;
             }
-        } catch(InputMismatchException e){
-            System.out.println("Invalid input. Please enter a valid number");
-            scanner.nextLine();
-            choice = -1;
-        } catch (Exception e){
-            System.out.println("An unexpected error occurred. Please try again");
-            scanner.nextLine();
-            choice = -1;
-        }
-        } while(choice != 10);
+        } while (choice != 10);
     }
 
-    private static void createSavingsAccount(){
-        try{
+    private static void createSavingsAccount() {
+        try {
             System.out.print("Enter account holder name: ");
             scanner.nextLine();
             String name = scanner.nextLine();
@@ -106,16 +106,16 @@ public class BankingApplication{
 
                 System.out.println("Savings Account created successfully. Account number: " + accountNumber);
             } else {
-                throw new BankingException("Insufficient initial deposit to meet the minimum balance requirement.");
+                throw new BankingException("Insufficient initial deposit to meet the minimum balance requirement");
             }
-            
+
         } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter valid numeric values.");
-            scanner.nextLine(); 
+            System.out.println("Invalid input. Please enter valid numeric values");
+            scanner.nextLine();
         } catch (BankingException e) {
             System.out.println("Error: " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("An unexpected error occurred. Please try again.");
+            System.out.println("An unexpected error occurred. Please try again");
             scanner.nextLine();
         }
     }
@@ -132,7 +132,7 @@ public class BankingApplication{
 
             double minimumBalance = 500;
             System.out.println("Minimum balance requirement for a Current Account: € " + minimumBalance);
-            
+
             System.out.print("Enter overdraft limit for the current account: € ");
             double overdraftLimit = scanner.nextDouble();
 
@@ -145,26 +145,26 @@ public class BankingApplication{
 
                 System.out.println("Current Account created successfully. Account number: " + accountNumber);
             } else {
-                throw new BankingException("Insufficient initial deposit to meet the minimum balance requirement.");
+                throw new BankingException("Insufficient initial deposit to meet the minimum balance requirement");
             }
 
         } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter valid numeric values.");
+            System.out.println("Invalid input. Please enter valid numeric values");
             scanner.nextLine();
         } catch (BankingException e) {
             System.out.println("Error: " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("An unexpected error occurred. Please try again.");
+            System.out.println("An unexpected error occurred. Please try again");
             scanner.nextLine();
         }
     }
 
-    private static void createSalaryAccount(){
+    private static void createSalaryAccount() {
         try {
             System.out.print("Enter account holder name: ");
             scanner.nextLine();
             String name = scanner.nextLine();
-            
+
             System.out.print("Enter initial deposit amount (€): ");
             double initialDeposit = scanner.nextDouble();
 
@@ -183,15 +183,15 @@ public class BankingApplication{
 
                 System.out.println("Salary Account created successfully. Account number: " + accountNumber);
             } else {
-                throw new BankingException("Insufficient initial deposit to meet the minimum balance requirement.");
+                throw new BankingException("Insufficient initial deposit to meet the minimum balance requirement");
             }
         } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter valid numeric values.");
+            System.out.println("Invalid input. Please enter valid numeric values");
             scanner.nextLine();
         } catch (BankingException e) {
             System.out.println("Error: " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("An unexpected error occurred. Please try again.");
+            System.out.println("An unexpected error occurred. Please try again");
             scanner.nextLine();
         }
     }
@@ -260,26 +260,44 @@ public class BankingApplication{
         }
     }
 
-    private static void deleteAcount(){
+    private static void deleteAcount() {
+        try {
+            System.out.print("Enter account number to delete: ");
+            int accountNumber = scanner.nextInt();
+
+            BankAccount account = findAccount(accountNumber);
+            if (account != null) {
+                accounts.remove(account);
+                System.out.println("Account deleted successfully");
+            } else {
+                System.out.println("Account not found with the given account number");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid account number");
+            scanner.nextLine();
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred. Please try again");
+            scanner.nextLine();
+        }
+    }
+
+    private static void deposit() {
 
     }
 
-    private static void deposit(){
+    private static void withdraw() {
 
     }
 
-    private static void withdraw(){
-
-    }
-
-    private static void searchAccount(){
+    private static void searchAccount() {
 
     }
 
     public static int generateAccountNumber() {
         return (int) (Math.random() * 90000) + 10000;
     }
-    
+
     private static BankAccount findAccount(int accountNumber) {
         for (BankAccount account : accounts) {
             if (account.getAccountNumber() == accountNumber) {
